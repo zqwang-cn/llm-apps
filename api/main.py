d2f5fdd7aa7b551cec7dbd6df5bb69ea5ab6a051
zqwang-cn <zqwang.cn@gmail.com>
2023-10-11 13:54:34 +0800

    add cors

--- api/main.py
+++ api/main.py
@@ -1,5 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import doc_qa
 
 app = FastAPI()
+origins = [
+    "*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(doc_qa.router)
